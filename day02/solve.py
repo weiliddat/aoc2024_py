54ca43776caef1f7cd9d108dc7af258460eb1895
chiawei <chiawei.ong@parcellab.com>
2024-12-02 18:05:03 +0100

    day02 part01

---
+++ day02/solve.py
@@ -0,0 +1,47 @@
+Input = list[list[int]]
+
+
+def part01(input: Input):
+    return sum([1 for report in input if report_safety(report)])
+
+
+def report_safety(report: list[int]) -> bool:
+    is_asc = None
+
+    for i in range(1, len(report)):
+        a = report[i - 1]
+        b = report[i]
+
+        if a == b:
+            return False
+        if abs(a - b) > 3:
+            return False
+
+        if is_asc is True and b < a:
+            return False
+        elif is_asc is False and a < b:
+            return False
+        elif is_asc is None:
+            is_asc = b > a
+
+    return True
+
+
+def part02(input: Input):
+    pass
+
+
+def parse_input(input: str) -> Input:
+    return [[int(i) for i in line.split()] for line in input.splitlines()]
+
+
+if __name__ == "__main__":
+    import os
+
+    dirname = os.path.dirname(__file__)
+    input_path = os.path.join(dirname, "input.txt")
+    with open(input_path) as input_file:
+        input = input_file.read()
+        parsed = parse_input(input)
+        print(part01(parsed))
+        print(part02(parsed))
---
+++ day02/test_solve.py
@@ -0,0 +1,34 @@
+from .solve import parse_input, part01, part02
+
+test_input = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+
+
+def test_parse_input():
+    expected = [
+        [7, 6, 4, 2, 1],
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+        [1, 3, 6, 7, 9],
+    ]
+    parsed = parse_input(test_input)
+    assert parsed == expected
+
+
+def test_part01():
+    expected = 2
+    actual = part01(parse_input(test_input))
+    assert actual == expected
+
+
+def test_part02():
+    expected = None
+    actual = part02(parse_input(test_input))
+    assert actual == expected
