e007cf1853e9949910ac29147eef1377142bac00
Chia Wei <ongchiawei@gmail.com>
2024-12-05 06:00:17 +0100

    wip day04

--- day04/solve.py
+++ day04/solve.py
@@ -1,24 +1,6 @@
 type Input = "Map"
 
 
-class Cursor:
-    map: "Map"
-    pos: tuple[int, int]
-    val: str
-
-    def __init__(self, map: "Map", start_pos: tuple[int, int]) -> None:
-        self.map = map
-        self.pos = start_pos
-
-    def move_and_get_value(self, relative_pos: tuple[int, int]) -> str | None:
-        rx, ry = relative_pos
-        new_pos = self.pos[0] + rx, self.pos[1] + ry
-        if new_val := self.map.at(new_pos):
-            self.pos = new_pos
-            self.val = new_val
-        return new_val
-
-
 class Map:
     data: list[str]
     width: int
@@ -29,9 +11,6 @@ class Map:
         self.width = len(lines[0])
         self.height = len(lines)
 
-    def get_cursor(self, start_pos: tuple[int, int] = (0, 0)):
-        return Cursor(self, start_pos)
-
     def at(self, pos: tuple[int, int]) -> str | None:
         x, y = pos
         if x < 0 or x >= self.width:
@@ -39,19 +18,28 @@ class Map:
         if y < 0 or y >= self.height:
             return None
         return self.data[y][x]
-    
+
     def search(self, v: str):
         for x in range(self.width):
             for y in range(self.height):
                 if self.at((x, y)) == v:
                     yield (x, y)
-        
+
+    def get_rows(self):
+        return self.data
+
+    def get_columns(self):
+        return ["".join(col) for col in zip(*self.data)]
+    
+    # gonna be lazy cos input is square
+    def get_diagonals(self):
+        pass
 
 
 def part01(input: Input):
-    starting_positions = input.search("X")
-    for pos in starting_positions:
-        print(pos)
+    print(input.width)
+    print(input.height)
+    print(input.get_columns())
 
 
 def part02(input: Input):
